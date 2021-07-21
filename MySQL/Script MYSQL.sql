cefed30b0b082c8207d79ab268ae31185350c8cc
Yuri Cunha <85268329+ycunha@users.noreply.github.com>
2021-07-21 06:36:08 -0300

    corrigindo alguns erros de portugues

--- MySQL/Script MYSQL.sql
+++ MySQL/Script MYSQL.sql
@@ -64,20 +64,17 @@ DESC CLIENTE;
 
 /*Tipos de dados */
 
-Todos os banos de dados possuem tipos que devem ser atribuidos aos dados de uma tabela.
+Todos os bancos de dados possuem tipos que devem ser atribuidos aos dados de uma tabela.
 Para caracteres literais, temos char e varchar, para números temos int e float, para
 objetos como fotos e documentos, temos o blob, para textos extensos, temos o text. 
 A disciplina de banco de dados é tão fantástica que ao entendermos o porque das coisas,
-podemos iniciar já em modo avançado e um bom exemplo disso são os tipos. Há uma profissão
-dentro da área que é a do analista de performance ou tuning, esse profissional é responsável
-por analisar o banco de dados e deixá-lo mais rápido. Parece algo avançado, e é! Porém,
-com alguas atitudes simples, podemos deixar o banco sem a necessidade de atuação desse profissional.
+podemos iniciar já em modo avançado e um bom exemplo disso são os tipos. 
 
 Cada caracter no banco de dados, vale 1 byte. Sendo assim, se eu entro com o dado JOÃO,
 estou entrando com 4 bytes no meu banco. E o que isso tem a ver com a tiagem de tabelas?
 
 O banco de dados funciona como um download de dados da internet. Se baixamos um arquivo de 1 giga,
-temos um temo maior que o download de 50 megas, considerando a mesma velocidade de conexão.
+temos um tempo maior que o download de 50 megas, considerando a mesma velocidade de conexão.
 
 Ao tiparmos uma tabela de modo errado ou displicente, vamos aumentar a quantidade de dados que 
 será baixada do banco de dados, prolongando assim o tempo de resposta.
@@ -92,11 +89,11 @@ char(10)
 entrando a palavra joao
 
 total de bytes varchar(10) = 4 bytes
-toal de bytes cahr(10) = 10 bytes
+total de bytes char(10) = 10 bytes
 
 isso ocorre pois o char não varia. Os caracteres restantes serao preenchidos com espaço. 
 eles nao ficam vazios. Enquanto que no varchar, o tipo varia conforme o dado.
-Entao utilizo sempre o varchar? Não. O charé ligeiramente mais performatico, por nao
+Entao utilizo sempre o varchar? Não. O char é ligeiramente mais performatico, por nao
 ter que gastar tempo variando de tamanho. Entao a regra é utilizar sempre o char quando
 sabemos que o numero de caracteres naquela coluna nao vai variar nunca. Por exemplo,
 unidade federativa, com dois digitos, sexo com um digito e assim por diante. Vista a diferença
